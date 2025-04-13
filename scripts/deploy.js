8af9b74bc5e1064f1c2b86cdc719dadc63a64d11
TechieTeee <techieteee@protonmail.com>
2025-04-13 06:21:11 +0000

    Updated deployment script to capture deployment details

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,9 @@ async function main() {
   const usdcAddress = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"; // Sepolia USDC
   const DataDocMarketplace = await hre.ethers.getContractFactory("DataDocMarketplace");
   const marketplace = await DataDocMarketplace.deploy(usdcAddress);
-  console.log("Deployed to Sepolia:", marketplace.address);
+  const deployedContract = await marketplace.waitForDeployment();
+  const address = await deployedContract.getAddress();
+  console.log("Deployed to Sepolia:", address);
 }
 
 main().catch(console.error);
