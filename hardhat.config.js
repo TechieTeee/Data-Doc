8554ded680e4ba2ca6357a38d5a4c90e13cf925d
TechieTeee <techieteee@protonmail.com>
2025-04-13 05:22:07 +0000

    Updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,7 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    holesky: {
-      url: "https://eth-holesky.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
